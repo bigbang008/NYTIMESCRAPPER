5ead1177c28ab5b8f8f1220f388d028c544e3d19
SarawutM <imagination.mystery@gmail.com>
2018-08-11 11:57:32 -0500

    first initial

---
+++ models/Article.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+//Update with correct info, add validation
+const Article = new Schema({
+    nyt_id: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    headline: {
+        type: String,
+        required: true,
+    },
+    byline: {
+        type: String,
+        required: true
+    },
+    pub_date: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        default: "No snippet available"
+    },
+    url: {
+        type: String,
+        required: true
+    }
+});
+
+
+module.exports = mongoose.model("Article", Article);
---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+	Article:require("./Article.js")
+}
+//add more models as you create them
